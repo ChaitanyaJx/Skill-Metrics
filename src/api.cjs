19dd35ae84b31347b2ad6aad216ff8beb82e508f
Chaitanya Jambhulkar <chaitanyajambhulkar768@gmail.com>
2024-09-29 16:16:25 +0530

    Update api.cjs

--- src/api.cjs
+++ src/api.cjs
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://skillmetrics.onrender.com/';
-const API_URL = "http://localhost:3000/";
+const API_URL = 'https://skillmetrics.onrender.com/';
+// const API_URL = "http://localhost:3000/";
 
 const api = axios.create({
   baseURL: API_URL,
