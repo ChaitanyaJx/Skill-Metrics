93026ec3e154682b1b4eed7c16cab3ee58399138
Chaitanya Jambhulkar <chaitanyajambhulkar768@gmail.com>
2024-09-29 14:39:53 +0530

    Update api.cjs

--- src/api.cjs
+++ src/api.cjs
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000'; // Replace with your backend URL
+const API_URL =  process.env.BACK_URL ||'http://localhost:3000'; // Replace with your backend URL
 
 const api = axios.create({
   baseURL: API_URL,
